f21ac2952f61c55e76009c7beb5e851d12aa4997
JiSoo Hyeon <jichuuu0417@gmail.com>
2022-08-25 21:31:25 +0900

    change project setting

--- rest_youtube_test/settings.py
+++ rest_youtube_test/settings.py
@@ -15,12 +15,25 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@1va0!hj6(j^+n!__#-v2b1d07e^%0nsd@@2(f*1)9-%b_0gy('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
